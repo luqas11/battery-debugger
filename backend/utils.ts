49dec545efdf04fee6af44a2409ce151f0277eec
luqas11 <lucascazas@hotmail.com>
2023-08-11 15:28:57 -0300

    Add JSDocs

--- backend/utils.ts
+++ backend/utils.ts
@@ -1,5 +1,8 @@
 import fs from "fs/promises";
 
+/**
+ * @returns the name of the current test in progress. If none is, returns undefined.
+ */
 export const getCurrentTestName = async () => {
   const recordsDir = await fs.readdir("../records");
   const fileName = recordsDir.find((file) => file.startsWith("current_"));
@@ -9,10 +12,19 @@ export const getCurrentTestName = async () => {
   return fileName.slice(8, -4);
 };
 
+/**
+ * @param name
+ * @returns the file name string for a test, given it's name.
+ */
 export const formatFileName = (name: string) => {
   return "current_" + name + ".csv";
 };
 
+/**
+ *
+ * @param name
+ * @returns wether a test with a given name exists or not.
+ */
 export const isTestNameAvailable = async (name: string) => {
   const recordsDir = await fs.readdir("../records");
   const fileName = recordsDir.find((file) => file === `${name}.csv`);
