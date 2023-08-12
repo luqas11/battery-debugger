28e547770114e57b237e335f339557b3d48439e3
luqas11 <lucascazas@hotmail.com>
2023-08-12 18:08:29 -0300

    Add voltage calculation

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,14 +1,29 @@
+// Built-in led pin number
 const int LED_PIN = 2;
+// Input voltage calibration value
+const float ref_voltage = 11.46;
+// Input ADC calibration value
+const int ref_adc = 566;
 
 void setup()
 {
     pinMode(LED_PIN, OUTPUT);
+    Serial.begin(115200);
 }
 
 void loop()
 {
     digitalWrite(LED_PIN, HIGH);
-    delay(1000);
+    delay(500);
     digitalWrite(LED_PIN, LOW);
-    delay(1000);
+    delay(500);
+
+    float inputValue = analogRead(A0);
+    float voltageValue = inputValue * (ref_voltage / ref_adc);
+    float currentTime = float(millis()) / (1000 * 60 * 60);
+
+    Serial.println("ADC input value: " + String(inputValue));
+    Serial.println("Calculated voltage value: " + String(voltageValue) + "V");
+    Serial.println("Running time: " + String(currentTime) + "h");
+    Serial.println();
 }
