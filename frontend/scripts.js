23586525c0d6510e2b8f6bba4f981a45e2f730fe
luqas11 <lucascazas@hotmail.com>
2023-08-14 13:03:16 -0300

    Configure Prettier as formatter for the frontend

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,30 +1,32 @@
 // Base URL of the backend
-const BASE_URL = 'http://192.168.0.11:3000'
+const BASE_URL = "http://192.168.0.11:3000";
 
 /**
  * Fetches the tests names list from the backend, and loads it into the test selector.
  */
 const fetchTestList = () => {
-    fetch(BASE_URL + "/get-test-list").then(response => {
-        if (!response.ok) {
-            throw new Error();
-        }
-        return response.json();
+  fetch(BASE_URL + "/get-test-list")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error();
+      }
+      return response.json();
     })
-    .then(data => {
-        const testSelector = document.getElementById("testSelector");
-        data.testNames.forEach((element, index, array) =>{
-            const newOption = document.createElement("option");
-            newOption.value = element;
-            newOption.textContent = element;
-            testSelector.appendChild(newOption);
-        });
-        testSelector.removeAttribute("disabled");  
+    .then((data) => {
+      const testSelector = document.getElementById("testSelector");
+      data.testNames.forEach((element, index, array) => {
+        const newOption = document.createElement("option");
+        newOption.value = element;
+        newOption.textContent = element;
+        testSelector.appendChild(newOption);
+      });
+      testSelector.removeAttribute("disabled");
     })
-    .catch(error => {
-        const errorMessage = document.getElementById("errorMessage");
-        errorMessage.style.display = "block";
-        errorMessage.textContent  = "The test list couldn't be retrieved. Try reloading the page, or checking the server status.";
+    .catch((error) => {
+      const errorMessage = document.getElementById("errorMessage");
+      errorMessage.style.display = "block";
+      errorMessage.textContent =
+        "The test list couldn't be retrieved. Try reloading the page, or checking the server status.";
     });
 };
 
@@ -32,27 +34,28 @@ const fetchTestList = () => {
  * Displays the graph for the selected test name on the page.
  */
 const getGraph = () => {
-    // Hide all error messages
-    const errorMessage = document.getElementById("errorMessage");
-    errorMessage.style.display = "none";
+  // Hide all error messages
+  const errorMessage = document.getElementById("errorMessage");
+  errorMessage.style.display = "none";
 
-    // Remove any graph being displayed
-    const targetDiv = document.getElementById("plotContainer");
-    if (targetDiv.firstChild) {
-        targetDiv.removeChild(targetDiv.firstChild);                
-    }
+  // Remove any graph being displayed
+  const targetDiv = document.getElementById("plotContainer");
+  if (targetDiv.firstChild) {
+    targetDiv.removeChild(targetDiv.firstChild);
+  }
 
-    // Show the selected graph
-    const testSelector = document.getElementById("testSelector");
-    const testName = testSelector.options[testSelector.selectedIndex]?.value;
-    const newImage = document.createElement("img");
-    newImage.src = `${BASE_URL}/records/${testName}`;  
-    targetDiv.appendChild(newImage);
-    newImage.onerror = () => {
-        newImage.remove();
-        errorMessage.style.display = "block";
-        errorMessage.textContent  = "The graph couldn't be loaded. Try reloading the page, or checking the server status.";
-    };
-}
+  // Show the selected graph
+  const testSelector = document.getElementById("testSelector");
+  const testName = testSelector.options[testSelector.selectedIndex]?.value;
+  const newImage = document.createElement("img");
+  newImage.src = `${BASE_URL}/records/${testName}`;
+  targetDiv.appendChild(newImage);
+  newImage.onerror = () => {
+    newImage.remove();
+    errorMessage.style.display = "block";
+    errorMessage.textContent =
+      "The graph couldn't be loaded. Try reloading the page, or checking the server status.";
+  };
+};
 
 fetchTestList();
