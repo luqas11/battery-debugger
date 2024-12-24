8f22122f217779f8e191e2957eab4aecd70820d7
luqas11 <lucascazas@hotmail.com>
2024-12-24 20:40:35 -0300

    Format firmware sketch with the Arduino IDE 2 auto formatter

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -11,36 +11,34 @@ const int REF_ADC = 566;
 // Readings period in minutes
 const int PERIOD = 2;
 
-void setup()
-{
-    // Setup initial parameters
-    pinMode(LED_PIN, OUTPUT);
-    Serial.begin(115200);
+void setup() {
+  // Setup initial parameters
+  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(115200);
 
-    // Try to connect to WiFi, and keep the LED on while trying
-    digitalWrite(LED_PIN, LOW);
-    connectToWifi();
-    digitalWrite(LED_PIN, HIGH);
+  // Try to connect to WiFi, and keep the LED on while trying
+  digitalWrite(LED_PIN, LOW);
+  connectToWifi();
+  digitalWrite(LED_PIN, HIGH);
 }
 
-void loop()
-{
-    // Read analog input and current time
-    float inputValue = analogRead(A0);
-    float voltageValue = inputValue * (REF_VOLTAGE / REF_ADC);
-    float currentTime = float(millis()) / (1000 * 60 * 60);
+void loop() {
+  // Read analog input and current time
+  float inputValue = analogRead(A0);
+  float voltageValue = inputValue * (REF_VOLTAGE / REF_ADC);
+  float currentTime = float(millis()) / (1000 * 60 * 60);
 
-    // Print those values to the serial monitor
-    Serial.println();
-    Serial.println("ADC input value: " + String(inputValue));
-    Serial.println("Calculated voltage value: " + String(voltageValue) + "V");
-    Serial.println("Running time: " + String(currentTime) + "h");
+  // Print those values to the serial monitor
+  Serial.println();
+  Serial.println("ADC input value: " + String(inputValue));
+  Serial.println("Calculated voltage value: " + String(voltageValue) + "V");
+  Serial.println("Running time: " + String(currentTime) + "h");
 
-    // Send the values to the BE
-    sendDataToBE(currentTime, voltageValue);
+  // Send the values to the BE
+  sendDataToBE(currentTime, voltageValue);
 
-    // Wait the indicated time to do take the next values
-    delay(PERIOD * 1000 * 60);
+  // Wait the indicated time to do take the next values
+  delay(PERIOD * 1000 * 60);
 }
 
 /**
@@ -48,23 +46,21 @@ void loop()
  * It will not return until a successful connection is established.
  * The network parameters are taken from the config.h file.
  */
-void connectToWifi()
-{
-    Serial.print("Connecting to ");
-    Serial.println(String(SSID));
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(String(SSID), String(PASSWORD));
+void connectToWifi() {
+  Serial.print("Connecting to ");
+  Serial.println(String(SSID));
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(String(SSID), String(PASSWORD));
 
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(500);
-        Serial.print(".");
-    }
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
 
-    Serial.println();
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
+  Serial.println();
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 /**
@@ -72,52 +68,40 @@ void connectToWifi()
  * The BE URL is taken from the config.h file.
  * After the request is sent, the led will blink indicating the result. A single blink inidcates success, and a double blink indicates failure.
  */
-void sendDataToBE(float time, float voltage)
-{
-    if (WiFi.status() == WL_CONNECTED)
-    {
-        WiFiClient client;
-        HTTPClient http;
+void sendDataToBE(float time, float voltage) {
+  if (WiFi.status() == WL_CONNECTED) {
+    WiFiClient client;
+    HTTPClient http;
 
-        http.begin(client, String(BASE_URL) + "/save-reading");
-        http.addHeader("Content-Type", "application/json");
-        int httpCode = http.POST("{\"voltage\":" + String(voltage) + ",\"time\":" + String(time) + "}");
-        if (httpCode == HTTP_CODE_OK)
-        {
-            blink(1);
-            Serial.println("Request successful");
-        }
-        else
-        {
-            blink(2);
-            if (httpCode < 0)
-            {
-                Serial.println("Request failed. Error: " + http.errorToString(httpCode) + ".");
-            }
-            else
-            {
-                Serial.println("Request failed. Error code: " + String(httpCode) + ".");
-            }
-        }
-        http.end();
+    http.begin(client, String(BASE_URL) + "/save-reading");
+    http.addHeader("Content-Type", "application/json");
+    int httpCode = http.POST("{\"voltage\":" + String(voltage) + ",\"time\":" + String(time) + "}");
+    if (httpCode == HTTP_CODE_OK) {
+      blink(1);
+      Serial.println("Request successful");
+    } else {
+      blink(2);
+      if (httpCode < 0) {
+        Serial.println("Request failed. Error: " + http.errorToString(httpCode) + ".");
+      } else {
+        Serial.println("Request failed. Error code: " + String(httpCode) + ".");
+      }
     }
-    else
-    {
-        blink(2);
-        Serial.println("Request not sent. Unable to connect to WiFi.");
-    }
+    http.end();
+  } else {
+    blink(2);
+    Serial.println("Request not sent. Unable to connect to WiFi.");
+  }
 }
 
 /**
  * Function to make the built-in LED blink a given amount of times
  */
-void blink(int times)
-{
-    for (int i = 0; i < times; i++)
-    {
-        digitalWrite(LED_PIN, LOW);
-        delay(250);
-        digitalWrite(LED_PIN, HIGH);
-        delay(250);
-    }
+void blink(int times) {
+  for (int i = 0; i < times; i++) {
+    digitalWrite(LED_PIN, LOW);
+    delay(250);
+    digitalWrite(LED_PIN, HIGH);
+    delay(250);
+  }
 }
